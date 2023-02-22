0da4b35fc18eed920b4716dc019e5cbd77cf8d20
choi <bcaca0831@gmail.com>
2023-02-22 10:02:15 +0900

    프로젝트 준비

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="inner">
+        <p>Copyright ⓒ choi All Right Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
