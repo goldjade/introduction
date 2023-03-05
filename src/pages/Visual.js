9f9d19db86596f1cef9b1af4ce0806ce8c279cd7
choi <bcaca0831@gmail.com>
2023-03-05 21:11:21 +0900

    visual page update

--- src/pages/Visual.js
+++ src/pages/Visual.js
@@ -1,9 +1,4 @@
-/** @format */
-
-import React, { useRef } from "react";
 // import Anime from "../assets/Anime";
-
-
 
 const Visual = () => {
   const path = process.env.PUBLIC_URL;
@@ -11,9 +6,18 @@ const Visual = () => {
   return (
     <div className="visual scroll">
       <div className="inner">
-        {/* 첫인상 남기기(사진/대표단어 ) */}
-        I
-        <img src={`${path}/images/visual.png`} alt="visual" />
+        <div className="introduction">
+          <span className="title">I CREATE WORLD</span>
+          <span className="sub-title">FRONTEND DEVELOPER</span>
+
+          <img src={`${path}/images/me.png`} alt="visual" className="me" />
+          <span className="name">CHOI.GEUM.OK</span>
+          <span className="greetings">
+            안녕하세요 <br />
+            세상 모든걸 구현하고 싶은 <br />
+            프론트앤드 개발자 최금옥 입니다.
+          </span>
+        </div>
       </div>
     </div>
   );
