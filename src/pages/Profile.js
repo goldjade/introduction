9a9a889f607803d9bdf9797a4d869400b333da71
choi <bcaca0831@gmail.com>
2023-03-19 23:01:16 +0900

    프로필 / 스킬 작업중

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from 'react';
+import React from "react";
 
 const Profile = () => {
   return (
@@ -21,41 +21,39 @@ const Profile = () => {
             <p>1991.08.31</p>
             <p>publisher & front-end</p>
             <p>bcaca0831@gmail.com</p>
-            <p>대충 깃허브랑 인스타그램이랑 넣자</p>
           </div>
           <div className="right">
-            <div>
+            <div className="top">
+              <p>꾸준하고 성실하게 진취적으로!</p>
               <p>
-                <span>꾸준하고 성실하게 진취적으로!</span>
-                <span>
-                  거북이가 토끼와의 경주에서 이길 수 있었던 이유는 쉬지않고
-                  끝까지 달렸기때문임 나는 꾸준하고 성실하게 성장해 나가는
-                  사람임 언젠가 그 누구보다 잘난놈이 될거임
-                </span>
+                거북이가 토끼와의 경주에서 이길 수 있었던 이유는 쉬지 않고
+                끝까지 달렸기 때문입니다.
+                <br />
+                저는 비록 조금 느릴지라도 꾸준하고 성실하게 성장해 나가는
+                사람입니다.
               </p>
             </div>
-            <div>
-              <p>Education</p>
-              <p>
+            <div className="bottom">
+              <div className="Education">
+                <p>Education</p>
                 <p>그린컴퓨터아트학원</p>
                 게임콘텐츠개발(MAX게임콘텐츠제작)양성훈련
                 <span>201710 201804 </span>
-              </p>
-              <p>
-                <p>그린컴퓨터아트학원</p>
-                기업 요구를 반영한 프로젝트 중심 프론트엔드 React(리액트)
-                개발자양성
-                <span>202210 - 202304</span>
-              </p>
-            </div>
-            <div>
-              <p>Experince</p>
-              <p>
-                희일커뮤니케이션 <span> 201108 - 201507</span>
-              </p>
-              <p>
-                주식회사 베스트(Best) <span> 201901 202203</span>
-              </p>
+                <p>
+                  기업 요구를 반영한 프로젝트 중심 프론트엔드 React(리액트)
+                  개발자양성
+                  <span>202210 - 202304</span>
+                </p>
+              </div>
+              <div className="Experince">
+                <p>Experince</p>
+                <p>
+                  희일커뮤니케이션 <span> 201108 - 201507</span>
+                </p>
+                <p>
+                  주식회사 베스트(Best) <span> 201901 202203</span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,9 +1,15 @@
 /** @format */
 
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Skills = () => {
   const path = process.env.PUBLIC_URL;
+
   return (
     <section className="skills scroll">
       <div className="inner">
@@ -49,78 +55,153 @@ const Skills = () => {
             </li>
           </ul>
           <ul className="skills-sub">
-            <li>
-              <img
-                src={`${path}/images/skill-git.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="700"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-redux.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="800"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-figma.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="900"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-type.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="1000"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-sass.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="1100"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-ps.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="1200"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-ai.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="1300"
-              />
-            </li>
-            <li>
-              <img
-                src={`${path}/images/skill-blender.png`}
-                alt="skill"
-                data-aos="fade-up"
-                data-aos-duration="300"
-                data-aos-delay="1400"
-              />
-            </li>
+            <Swiper
+              style={{ height: "280px", padding: "0 100px 0 100px" }}
+              spaceBetween={150}
+              slidesPerView={3}
+              onSlideChange={() => console.log("slide change")}
+              pagination={{ clickable: true }}
+              navigation={true}
+              loop={true}
+              autoplay={true}
+              breakpoints={{
+                860: {
+                  slidesPerView: 4,
+                  spaceBetween: 30,
+                },
+                960: {
+                  slidesPerView: 4,
+                  spaceBetween: 50,
+                },
+                1180: {
+                  slidesPerView: 5,
+                  spaceBetween: 150,
+                },
+              }}
+              modules={[Navigation, Pagination, Autoplay]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="700"
+                >
+                  <img src={`${path}/images/skill-ps.png`} alt="skill" />
+                  <p>photoshp</p>
+                  <div className="progress-bar">
+                    <div className="ps"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="800"
+                >
+                  <img src={`${path}/images/skill-ai.png`} alt="skill" />
+                  <p>illustrator</p>
+                  <div className="progress-bar">
+                    <div className="ai"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="900"
+                >
+                  <img src={`${path}/images/skill-figma.png`} alt="skill" />
+                  <p>pigma</p>
+                  <div className="progress-bar">
+                    <div className="pigma"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="1000"
+                >
+                  <img
+                    src={`${path}/images/skill-tailwindcss.png`}
+                    alt="skill"
+                  />
+                  <p>tailwindcss</p>
+                  <div className="progress-bar">
+                    <div className="tailwindcss"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="1100"
+                >
+                  <img src={`${path}/images/skill-sass.png`} alt="skill" />
+                  <p>sass</p>
+                  <div className="progress-bar">
+                    <div className="sass"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="1200"
+                >
+                  <img src={`${path}/images/skill-git.png`} alt="skill" />
+                  <p>github</p>
+                  <div className="progress-bar">
+                    <div className="git"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="1300"
+                >
+                  <img src={`${path}/images/skill-jquery.png`} alt="skill" />
+                  <p>jQuery</p>
+                  <div className="progress-bar">
+                    <div className="jquery"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="1400"
+                >
+                  <img src={`${path}/images/skill-type.png`} alt="skill" />
+                  <p>typescript</p>
+                  <div className="progress-bar">
+                    <div className="typescript"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+              <SwiperSlide>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="300"
+                  data-aos-delay="1500"
+                >
+                  <img src={`${path}/images/skill-redux.png`} alt="skill" />
+                  <p>redux</p>
+                  <div className="progress-bar">
+                    <div className="redux"></div>
+                  </div>
+                </li>
+              </SwiperSlide>
+            </Swiper>
           </ul>
         </div>
       </div>
