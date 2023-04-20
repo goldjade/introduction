92c3367ad87326a1f1c9d83db3c669fd93318cff
choi <bcaca0831@gmail.com>
2023-04-20 11:57:10 +0900

    링크 수정

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -54,14 +54,6 @@ const Portfolio = ({ path }) => {
                       <li className="period">
                         2023 . 01 . 11 - 2023 . 02 . 06
                       </li>
-
-                      <li className="sub-title">구현 기능</li>
-                      <li>회원 가입 / 정보수정 / 회원 탈퇴 가능</li>
-                      <li>매장 위치 / 길찾기 </li>
-                      <li>
-                        메뉴 / 장바구니 / 주문하기 / 주문내역 / 주소정보 /
-                        마이배달지
-                      </li>
                       <li className="sub-title">기여도</li>
                       <li>
                         회원기능 (로그인/로그인 인증/회원 정보 수정 / 회원 탈퇴)
@@ -142,16 +134,6 @@ const Portfolio = ({ path }) => {
                       <li className="title">나의 식단일지</li>
                       <li className="period">
                         2023 . 02 . 08 - 2023 . 03 . 03{" "}
-                      </li>
-
-                      <li className="sub-title">구현 기능</li>
-                      <li>회원 가입 / 정보수정 / 회원 탈퇴 가능</li>
-                      <li>
-                        샘플 식단 등록 / 직접 식단 등록 / 식단 수정 / 식단 삭제{" "}
-                      </li>
-                      <li>
-                        목표 칼로리 달성여부 / 이전 식단 확인 섭취 약물 등록 /
-                        수정 / 삭제
                       </li>
 
                       <li className="sub-title">기여도</li>
@@ -237,22 +219,12 @@ const Portfolio = ({ path }) => {
                       <li className="period">
                         2023 . 02 . 08 - 2023 . 03 . 03{" "}
                       </li>
-
-                      <li className="sub-title">구현 기능</li>
-                      <li>회원 가입 / 정보수정 / 회원 탈퇴 가능</li>
-                      <li>
-                        샘플 식단 등록 / 직접 식단 등록 / 식단 수정 / 식단 삭제{" "}
-                      </li>
-                      <li>
-                        목표 칼로리 달성여부 / 이전 식단 확인 섭취 약물 등록 /
-                        수정 / 삭제
-                      </li>
-
                       <li className="sub-title">기여도</li>
                       <li>
-                        디자인 / 식단기능 (식단 등록/수정/삭제 / 칼로리계산 /
-                        목표달성 여부 확인)
+                        디자인 / 차트 기능( 성적 관리 및 테스트 차트 제작 /
+                        주간,월간 성적 확인 / 데이터 그래프 출력)
                       </li>
+
                       <li className="sub-title">프로젝트 도구</li>
                       <div className="tool-img">
                         <img
@@ -371,11 +343,10 @@ const Portfolio = ({ path }) => {
                 >
                   <img src={`${path}/images/study1.jpg`} alt="portfolio" />
                 </a>
-              
               </SwiperSlide>
               <SwiperSlide>
                 <a
-                  href="https://goldjade.github.io/stx/"
+                  href="https://goldjade.github.io/hansalim/"
                   target="_blank"
                   rel="noopener noreferrer"
                   alt="portfolio"
@@ -385,7 +356,7 @@ const Portfolio = ({ path }) => {
               </SwiperSlide>
               <SwiperSlide>
                 <a
-                  href="https://goldjade.github.io/stx/"
+                  href="https://goldjade.github.io/hyundai-clone"
                   target="_blank"
                   rel="noopener noreferrer"
                   alt="portfolio"
@@ -418,13 +389,13 @@ const Portfolio = ({ path }) => {
               <div className="ect-list-img">
                 <img src={`${path}/images/ect5.jpg`} alt="portfolio" />
                 <p className="neyong">
-                <span>#패키지 디자인 #배너이미지</span>
+                  <span>#패키지 디자인 #배너이미지</span>
                 </p>
               </div>
               <div className="ect-list-img">
                 <img src={`${path}/images/ect6.jpg`} alt="portfolio" />
                 <p className="neyong">
-                <span>#배너이미지</span>
+                  <span>#배너이미지</span>
                 </p>
               </div>
               <div className="ect-list-img">
